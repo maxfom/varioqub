43873e29e715cde58ec0c0ee767e6119e23aeacf
maksim.fomichev@r1.team <maksim.fomichev@r1.team>
2024-08-30 17:08:44 +0300

    varioqub only

--- Package.swift
+++ Package.swift
@@ -1,43 +1,23 @@
-// swift-tools-version:5.10
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "Varioqub",
     platforms: [
-        .iOS(.v12) // Ensures compatibility with iOS 11 and later
+        .iOS(.v13) // Ensures compatibility with iOS 13 and later
     ],
     products: [
         .library(
             name: "Varioqub",
             targets: ["Varioqub"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/yandexmobile/metrica-sdk-ios", from: "4.5.2"), // YandexMobileMetrica
-        // Add any other dependencies required here
-    ],
     targets: [
-        .binaryTarget(
-            name: "MetricaAdapter",
-            path: "MetricaAdapter.xcframework" // Adjust the path to where the .xcframework is located
-        ),
-        .binaryTarget(
-            name: "MetricaAdapterReflection",
-            path: "MetricaAdapterReflection.xcframework" // Adjust the path to where the .xcframework is located
-        ),
-        .binaryTarget(
-            name: "VQSwiftProtobuf",
-            path: "VQSwiftProtobuf.xcframework" // Adjust the path to where the .xcframework is located
-        ),
         .binaryTarget(
             name: "VarioqubMain",
             path: "Varioqub.xcframework" // Adjust the path to where the .xcframework is located
         ),
         .target(
             name: "Varioqub",
-            dependencies: [
-                .product(name: "YandexMobileMetrica", package: "metrica-sdk-ios"), // Explicit declaration of the product and package
-                "MetricaAdapter" // Ensure this exists or is linked correctly
-            ],
             path: ""
         ),
     ]
